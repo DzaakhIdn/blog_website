57a7aec699907e516f7e8c070ef60b39a7adef39
DzaakhIdn <dzaakhidn@gmail.com>
2024-11-07 18:51:37 +0700

    "malam"

--- src/js/custom/main.js
+++ src/js/custom/main.js
@@ -11,8 +11,7 @@ $(document).ready(function(){
                     slidesToShow: 2,   
                     slidesToScroll: 1,
                     infinite: true,
-                    autoplay: true,
-                    autoplaySpeed: 4000
+                    autoplay: false
                 }
             },
             {
@@ -29,14 +28,52 @@ $(document).ready(function(){
     });
 
     $('.slider').slick({
-        infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        dots: true,          
-        arrows: false,        
         autoplay: true,
-        autoplaySpeed: 4000
-      });
+        autoplaySpeed: 4000,
+        prevArrow: '#btn_left',
+        nextArrow: '#btn_right'
+    });
+    $('.mobile_tags').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4500
+    });
+
+    $('.large_tags').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: false,
+        prevArrow: '#prev_tags',
+        nextArrow: '#next_tags'
+    });
+
+    function checkArrow(currentSlides, totalSlides){
+        if(currentSlides == 0){
+            $('#prev_tags').prop('disabled', true).addClass('hidden')
+            $('.penutup_kiri').prop('disabled', true).addClass('hidden')
+        }else{
+            $('#prev_tags').prop('disabled', false).removeClass('hidden')
+            $('.penutup_kiri').prop('disabled', false).removeClass('hidden')
+        }
+
+        if(currentSlides == totalSlides - 1){
+            $('#next_tags').prop('disabled', true).addClass('hidden')
+            $('.penutup_kanan').prop('disabled', true).addClass('hidden')
+        }else{
+            $('#next_tags').prop('disabled', false).removeClass('hidden')
+            $('.penutup_kanan').prop('disabled', false).removeClass('hidden')
+        }
+    }
+
+    $('.large_tags').on('afterChange', function(event, slick, currentSlides){
+        const totalSlides = slick.slideCount;
+        checkArrow(currentSlides, totalSlides)
+    });
+
+    checkArrow(0, $('.large_tags').slick('getSlick').slideCount);
 
     $('#btn_next').on("click", function(){
         $('.carrousel_post').slick('slickNext')
